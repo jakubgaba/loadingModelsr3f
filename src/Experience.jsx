2beeb4646ba45b6efbe2f885fb35d42b8e03c24c
Jakub <j.gaba@email.cz>
2024-01-05 21:02:23 +0100

    ExploringNetworkThrottlingHah

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -10,7 +10,7 @@ export default function Experience() {
 
     const model = useLoader(
         GLTFLoader,
-        './hamburger-draco.glb',
+        './FlightHelmet/glTF/FlightHelmet.gltf',
         (loader) => {
             const dracoLoader = new DRACOLoader()
             dracoLoader.setDecoderPath('./draco/')
@@ -27,21 +27,12 @@ export default function Experience() {
         <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} />
 
-        <mesh castShadow position-x={- 2}>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={2} scale={1.5}>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
 
         <mesh receiveShadow position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
 
-        <primitive object={model.scene} />
+        <primitive object={model.scene} scale={5} position-y={-1} />
     </>
 }
